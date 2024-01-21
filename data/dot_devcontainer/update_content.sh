c469c0d1c37037882977a5f5d78b0714398842fb
Mathias Kende <mathias@kende.fr>
2024-01-21 17:35:18 +0000

    Install some common perl utilities in a devcontainer.

--- data/dot_devcontainer/update_content.sh
+++ data/dot_devcontainer/update_content.sh
@@ -4,3 +4,7 @@
 
 sudo cpanm --notest --installdeps --with-develop --with-configure --with-recommends --with-suggests --with-all-features .
 perl Makefile.PL
+
+if [ -n "${PAUSE_USERNAME}" ] && [ -n "${PAUSE_USERNAME}" ]; then
+  echo "user ${PAUSE_USERNAME}\npassword ${PAUSE_PASSWORD}" > ~/.pause
+fi
