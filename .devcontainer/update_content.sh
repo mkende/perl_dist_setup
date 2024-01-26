9aa35a3d948aca27cac1ba5696520f8d23331b7f
Mathias Kende <mathias@kende.fr>
2024-01-26 22:27:19 +0000

    Add an empty line before the template footers.

--- .devcontainer/update_content.sh
+++ .devcontainer/update_content.sh
@@ -1,5 +1,6 @@
 # DO NOT EDIT! This file is written by perl_setup_dist.
 # If needed, you can add content at the end of the file.
+
 # This script is executed during the updateContent phase of the container lifecycle.
 # It contains all the commands that need access to the repository content and so,
 # cannot be executed as part of the Dockerfile.
@@ -10,4 +11,5 @@ perl Makefile.PL
 if [ -n "${PAUSE_USERNAME}" ] && [ -n "${PAUSE_USERNAME}" ]; then
   echo "user ${PAUSE_USERNAME}\npassword ${PAUSE_PASSWORD}" > ~/.pause
 fi
+
 # End of the template. You can add custom content below this line.
